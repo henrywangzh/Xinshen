df4ce1a6f3cc8542038379c570c8470ef9dd9325
Sinkasha <xusherri@gmail.com>
2023-02-04 19:48:45 -0800

    Flow Evade should work now

--- Xinshen/Assets/Scripts/Player/FlowEvade.cs
+++ Xinshen/Assets/Scripts/Player/FlowEvade.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,23 @@ using UnityEngine;
 public class FlowEvade : MonoBehaviour
 {
     FlowScriptController controller;
+    Rigidbody rb;
+    Vector3 destination;
+    
+    [SerializeField] float dashSpeed;
     
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<FlowScriptController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        rb.velocity = transform.forward * dashSpeed;
+
+        // Now we've totally evaded
     }
 
     // Update is called once per frame
