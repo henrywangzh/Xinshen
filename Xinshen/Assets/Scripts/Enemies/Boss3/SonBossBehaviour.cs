965aed456741d9cfac70804d4d17b1777bb5093e
lzhou0714 <lzhou0714@g.ucla.edu>
2023-03-04 20:11:14 -0800

    look at player

--- Xinshen/Assets/Scripts/Enemies/Boss3/SonBossBehaviour.cs
+++ Xinshen/Assets/Scripts/Enemies/Boss3/SonBossBehaviour.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class SonBossBehaviour : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    
+
+    public float LookAtSpeed;
+    public float approachSpeed;
+    public float decisionTime;
+    private Coroutine LookCoroutine;
     private Rigidbody rb;
+    private float startTime;
     Collider col;
     Animator anim;
     // Start is called before the first frame update
@@ -15,12 +21,25 @@ public class SonBossBehaviour : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         col = GetComponent<Collider>();
+        startTime = Time.timeSinceLevelLoad;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // face player
+        transform.forward += ((player.position - transform.position) - transform.forward) * LookAtSpeed;
+        elapsed = (Time.timeSinceLevelLoad - startTime) % decisionPeriod
+        if (elapsed >= decisionTime)
+        {
+            AttackPlayer();
+        }
+
+    }
+
+    public void AttackPlayer()
+    {
+        transform.position = approachSpeed * (player.position - transform.position);
     }
 }
