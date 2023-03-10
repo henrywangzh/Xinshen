62ccf3e1adc2f9d0b8316d29bcbe19d4882d991a
henrywangzh <henrywang3@g.ucla.edu>
2023-03-10 13:10:43 -0800

    working on multi floor procedural generation

--- Xinshen/Assets/ProceduralGenerationV1.cs
+++ Xinshen/Assets/ProceduralGenerationV1.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ProceduralGenerationV1 : MonoBehaviour
@@ -8,39 +9,44 @@ public class ProceduralGenerationV1 : MonoBehaviour
     [SerializeField] GameObject hallwayObj;
     [SerializeField] GameObject[] wallObjs;
     [SerializeField] GameObject[] doorwayObjs;
+    [SerializeField] GameObject windTunnel;
     //[SerializeField] int targetRoomCount; //NOT IMPLEMENTED
     [SerializeField] float spawnChance, spawnChanceDecrRate, linearity, yElevation;
+    float startingSpawnChance;
     enum algorithm {DFS, BFS };
     [SerializeField] algorithm currentAlgorithm;
     //linearity: 0 - 1.0 denoting how linear the dungeon will be (e.g. proportion of BFS to DFS. 0.4 = 40% DFS)
     [SerializeField] int gridSpaceSize;
+    [SerializeField] int floors = 1;
     [SerializeField] int linearityPersistence = 5;
     int currentLinearityPersistence;
 
-    List<Room> rooms = new List<Room>();
+    List<List<Room>> allRooms = new List<List<Room>>();
     List<Room> ungeneratedRooms = new List<Room>();
+    List<Room> rooms;
 
-    const int ROOM_8x8 = 0, ROOM__16x16 = 1, ROOM__24x24 = 2;
+    const int ROOM_8x8 = 0, ROOM__16x16 = 1, ROOM__24x24 = 2, ROOM__16x16__STAIR = 3;
     const int HALLWAY_2 = 0, HALLWAY_10 = 1, HALLWAY_18 = 2;
     const int POS_X = 0, NEG_X = 1, POS_Y = 2, NEG_Y = 3;
     const int UNAVAILABLE = 1, AVAILABLE = 0, SELECTED = 2, OPENED = 3;
 
     class Room
     {
-        public Room(int pRoomSize, Vector2 pgridPosition)
+        public Room(int pRoomSize, Vector2 pgridPosition, float yPosition)
         {
             roomSize = pRoomSize;
             gridPosition = pgridPosition;
+            this.yPosition = yPosition;
         }
-
+        public float yPosition;
         public int roomSize;
         public Vector2 gridPosition;
         public int[] nodeStatus = new int[4];
     }
 
-    Vector3 GridToWorldCoordinates(Vector2 coords)
+    Vector3 GridToWorldCoordinates(Vector2 coords, float yPosition)
     {
-        return new Vector3(coords.x * gridSpaceSize, yElevation, coords.y * gridSpaceSize);
+        return new Vector3(coords.x * gridSpaceSize, yPosition, coords.y * gridSpaceSize);
     }
     Vector2 WorldTogridPosition(Vector3 coords)
     {
@@ -51,22 +57,68 @@ public class ProceduralGenerationV1 : MonoBehaviour
 
     void Start()
     {
-        currentLinearityPersistence = linearityPersistence;
-        Room newRoom = new Room(ROOM__16x16, Vector2.zero);
-        for (int i = 0; i < 4; i++)
+        startingSpawnChance = spawnChance;
+        for(int i = 0; i < floors; i++)
         {
-            newRoom.nodeStatus[i] = UNAVAILABLE;
+            allRooms.Add(new List<Room>());
+            rooms = allRooms[i];
+            currentLinearityPersistence = linearityPersistence;
+            Room startingRoom;
+            if(i == 0)
+            {
+                startingRoom = new Room(ROOM__16x16, Vector2.zero, yElevation);
+
+            } else
+            {
+                List<Room> potentialStairRooms = allRooms[i - 1].FindAll(room => room.roomSize == ROOM__16x16);
+                Room stairRoom = potentialStairRooms[Random.Range(0, potentialStairRooms.Count)];
+                
+                Vector2 startingPoint = stairRoom.gridPosition;
+                startingRoom = new Room(ROOM__16x16__STAIR, startingPoint, yElevation);
+
+            }
+            for (int j = 0; j < 4; j++)
+            {
+                startingRoom.nodeStatus[j] = UNAVAILABLE;
+            }
+            startingRoom.nodeStatus[POS_Y] = AVAILABLE;
+
+            rooms.Add(startingRoom);
+            GenerateRoomsFromRoom(startingRoom);
+            yElevation += 10;
+            spawnChance = startingSpawnChance;
         }
-        newRoom.nodeStatus[POS_Y] = AVAILABLE;
-
-        rooms.Add(newRoom);
-
-        GenerateRoomsFromRoom(newRoom);
+       
     }
 
     int roomIndex;
     void FixedUpdate()
     {
+        //for(int x = 0; x < floors; x++)
+        //{
+        //    rooms = allRooms[x];
+        //    if (roomIndex < rooms.Count)
+        //    {
+        //        InstantiateRoomObject(rooms[roomIndex]);
+        //        InstantiateWalls(rooms[roomIndex]);
+
+        //        for (int i = 0; i < 4; i++)
+        //        {
+        //            if (rooms[roomIndex].nodeStatus[i] == SELECTED)
+        //            {
+        //                InstantiateHallway(rooms[roomIndex], GetRoomAtPosition(rooms[roomIndex].gridPosition + DirectionToGridVector(i)));
+        //                //InstantiateHallway(rooms[roomIndex], i);
+        //            }
+        //        }
+        //        roomIndex++;
+        //    }
+        //    else
+        //    {
+
+        //    }
+        //}
+        rooms = allRooms.SelectMany(x => x).ToList();
+
         if (roomIndex < rooms.Count)
         {
             InstantiateRoomObject(rooms[roomIndex]);
@@ -81,10 +133,6 @@ public class ProceduralGenerationV1 : MonoBehaviour
                 }
             }
             roomIndex++;
-        }
-        else
-        {
-
         }
     }
 
@@ -112,7 +160,7 @@ public class ProceduralGenerationV1 : MonoBehaviour
 
                     if (newRoom == null)
                     {
-                        newRoom = new Room(Random.Range(0,3), room.gridPosition + DirectionToGridVector(randomIndex));
+                        newRoom = new Room(Random.Range(0,3), room.gridPosition + DirectionToGridVector(randomIndex), room.yPosition);
                         newRooms.Add(newRoom);
                         newRoom.nodeStatus[GetOppositeNode(randomIndex)] = OPENED;
                         //InstantiateRoomObject(newRoom);
@@ -215,18 +263,18 @@ public class ProceduralGenerationV1 : MonoBehaviour
 
     void InstantiateRoomObject(Room room)
     {
-        Instantiate(roomObjs[room.roomSize], GridToWorldCoordinates(room.gridPosition), Quaternion.identity);
+        Instantiate(roomObjs[room.roomSize], GridToWorldCoordinates(room.gridPosition, room.yPosition), Quaternion.identity);
     }
 
     void InstantiateHallway(Room room, int direction)
     {
         if (direction == POS_Y || direction == NEG_Y)
         {
-            Instantiate(hallwayObj, GridToWorldCoordinates(GetHallwayPositionInDirection(room.gridPosition, direction)), Quaternion.identity).transform.Rotate(Vector3.up * 90);
+            Instantiate(hallwayObj, GridToWorldCoordinates(GetHallwayPositionInDirection(room.gridPosition, direction), room.yPosition), Quaternion.identity).transform.Rotate(Vector3.up * 90);
         }
         else
         {
-            Instantiate(hallwayObj, GridToWorldCoordinates(GetHallwayPositionInDirection(room.gridPosition, direction)), Quaternion.identity);
+            Instantiate(hallwayObj, GridToWorldCoordinates(GetHallwayPositionInDirection(room.gridPosition, direction), room.yPosition), Quaternion.identity);
         }
     }
     void InstantiateHallway(Room room1, Room room2)
@@ -239,14 +287,14 @@ public class ProceduralGenerationV1 : MonoBehaviour
 
             if (room2.gridPosition.y - room1.gridPosition.y > 0)
             {
-                hallwayTrfm.position = GridToWorldCoordinates((GetRoomEdge(room2, NEG_Y) + GetRoomEdge(room1, POS_Y)) / 2);
+                hallwayTrfm.position = GridToWorldCoordinates((GetRoomEdge(room2, NEG_Y) + GetRoomEdge(room1, POS_Y)) / 2, room1.yPosition);
 
                 float length = (GetRoomEdge(room2, NEG_Y).y - GetRoomEdge(room1, POS_Y).y) * gridSpaceSize;
                 hallwayTrfm.localScale = new Vector3(length, 1, 5);
             }
             else
             {
-                hallwayTrfm.position = GridToWorldCoordinates((GetRoomEdge(room2, POS_Y) + GetRoomEdge(room1, NEG_Y)) / 2);
+                hallwayTrfm.position = GridToWorldCoordinates((GetRoomEdge(room2, POS_Y) + GetRoomEdge(room1, NEG_Y)) / 2, room1.yPosition);
 
                 float length = (GetRoomEdge(room1, NEG_Y).y - GetRoomEdge(room2, POS_Y).y) * gridSpaceSize;
                 hallwayTrfm.localScale = new Vector3(length, 1, 5);
@@ -256,14 +304,14 @@ public class ProceduralGenerationV1 : MonoBehaviour
         {
             if (room2.gridPosition.x - room1.gridPosition.x > 0)
             {
-                hallwayTrfm.position = GridToWorldCoordinates((GetRoomEdge(room2, NEG_X) + GetRoomEdge(room1, POS_X)) / 2);
+                hallwayTrfm.position = GridToWorldCoordinates((GetRoomEdge(room2, NEG_X) + GetRoomEdge(room1, POS_X)) / 2, room1.yPosition);
 
                 float length = (GetRoomEdge(room2, NEG_X).x - GetRoomEdge(room1, POS_X).x) * gridSpaceSize;
                 hallwayTrfm.localScale = new Vector3(length, 1, 5);
             }
             else
             {
-                hallwayTrfm.position = GridToWorldCoordinates((GetRoomEdge(room2, POS_X) + GetRoomEdge(room1, NEG_X)) / 2);
+                hallwayTrfm.position = GridToWorldCoordinates((GetRoomEdge(room2, POS_X) + GetRoomEdge(room1, NEG_X)) / 2, room1.yPosition);
 
                 float length = (GetRoomEdge(room1, NEG_X).x - GetRoomEdge(room2, POS_X).x) * gridSpaceSize;
                 hallwayTrfm.localScale = new Vector3(length, 1, 5);
@@ -294,14 +342,20 @@ public class ProceduralGenerationV1 : MonoBehaviour
         Transform wallTrfm;
         for (int i = 0; i < 4; i++)
         {
+            int roomSize = room.roomSize;
+            if (doorwayObjs.Count() < room.roomSize)
+            {
+                roomSize = room.roomSize - 1;
+            }
             if (room.nodeStatus[i] == SELECTED || room.nodeStatus[i] == OPENED)
             {
-                wallTrfm = Instantiate(doorwayObjs[room.roomSize], GridToWorldCoordinates(GetRoomEdge(room, i)) + Vector3.up * 2, Quaternion.identity).transform;
+                
+                wallTrfm = Instantiate(doorwayObjs[roomSize], GridToWorldCoordinates(GetRoomEdge(room, i), room.yPosition) + Vector3.up * 2, Quaternion.identity).transform;
                 if (i == POS_Y || i == NEG_Y) { wallTrfm.Rotate(Vector3.up * 90); }
             }
             else
             {
-                wallTrfm = Instantiate(wallObjs[room.roomSize], GridToWorldCoordinates(GetRoomEdge(room, i)) + Vector3.up * 2, Quaternion.identity).transform;
+                wallTrfm = Instantiate(wallObjs[roomSize], GridToWorldCoordinates(GetRoomEdge(room, i), room.yPosition) + Vector3.up * 2, Quaternion.identity).transform;
                 if (i == POS_Y || i == NEG_Y) { wallTrfm.Rotate(Vector3.up * 90); }
             }
         }
