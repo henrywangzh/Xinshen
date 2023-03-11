6e80e500954f26d5ca7c63f144b652ea39e9bea5
Do3naut <51458829+Do3naut@users.noreply.github.com>
2023-03-10 16:47:34 -0800

    playtesting manager

--- Xinshen/Assets/Scripts/Player/FlowEvade.cs
+++ Xinshen/Assets/Scripts/Player/FlowEvade.cs
@@ -82,13 +82,13 @@ public class FlowEvade : MonoBehaviour
     void Update()
     {
         currentDashTime += Time.deltaTime;
-        if(currentDashTime >= totalDashTime || Input.GetKeyUp(KeyCode.LeftShift))
+        if(currentDashTime >= totalDashTime || (!Input.GetKey(KeyCode.LeftShift) && currentDashTime >= totalDashTime / 2f))
         {
             move.SetDashCD(dashCD);
             controller.switchState.Invoke("move");
             anim.Play("FlowMove");
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
         {
             // controller.switchState.Invoke("move");
         }
---
+++ Xinshen/Assets/Scripts/PlaytestingManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaytestingManager : MonoBehaviour
+{
+    [SerializeField] bool isBossRoom = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            if (isBossRoom)
+                SceneManager.LoadScene("FirstLevel");
+            else
+                SceneManager.LoadScene("MingTest");
+        } else if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            if (isBossRoom)
+                SceneManager.LoadScene("MingTest");
+            else
+                SceneManager.LoadScene("FirstLevel");
+        }
+
+
+    }
+}
