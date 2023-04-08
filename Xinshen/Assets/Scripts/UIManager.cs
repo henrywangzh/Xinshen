b958cdb9d4c3f42c7270effff267511849bc2f00
Channa188 <clea_h@p-hall.net>
2023-04-08 16:12:09 -0700

    UI Update
    Some very basic UI setup

---
+++ Xinshen/Assets/Scripts/UIManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public int FirstLevel, UI;
+    public GameObject MainMenu, CreditMenu, SettingsMenu, PauseMenu;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FirstLevel");
+
+    }
+
+    public void Settings()
+    {
+        MainMenu.SetActive(false);
+        CreditMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+
+    public void Credit()
+    {
+        MainMenu.SetActive(false);
+        CreditMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
+    }
+
+    public void Back()
+    {
+        MainMenu.SetActive(true);
+        CreditMenu.SetActive(false);
+        SettingsMenu.SetActive(false);
+        //pauseScreen.SetActive(false);
+        //PauseButton.SetActive(false);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        SceneManager.LoadScene("UI");
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        PauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        PauseMenu.SetActive(false);
+    }
+}
