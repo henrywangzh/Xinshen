ea0b534f7a2fc4b81ac107c9d071c6a600a4f5db
henrywangzh <henrywang3@g.ucla.edu>
2023-04-29 17:13:29 -0700

    barrels and crates done

--- Xinshen/Assets/Shatter.cs
+++ Xinshen/Assets/Shatter.cs
@@ -1,34 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Shatter : Enemy
 {
     [SerializeField] GameObject shatteredVersion;
     [SerializeField] GameObject parent;
 
+    [SerializeField] float power = 500;
+
+    [SerializeField]
+    private float PieceSleepCheckDelay = 0.1f;
+
+    [SerializeField]
+    private float PieceDestroyDelay = 5f;
+
+    [SerializeField]
+    private float PieceFadeSpeed = 0.25f;
+
+
     public override void Die()
     {
+        Destroy(GetComponent<Rigidbody>());
+        GetComponent<Collider>().enabled = false;
+        GetComponent<Renderer>().enabled = false;
+
         GameObject shatteredObject = Instantiate(shatteredVersion, transform.position, Quaternion.identity);
 
         Rigidbody[] rigidbodies = shatteredObject.GetComponentsInChildren<Rigidbody>();
         foreach(Rigidbody rb in rigidbodies)
         {
-                rb.AddExplosionForce(Random.Range(500, 1000), transform.position, 10);
+                rb.AddExplosionForce(power * UnityEngine.Random.Range(0.5f, 1.5f), transform.position, 10);
+        }
+        StartCoroutine(FadeOutRigidBodies(rigidbodies));
+    }
+
+    private IEnumerator FadeOutRigidBodies(Rigidbody[] Rigidbodies)
+    {
+        WaitForSeconds Wait = new WaitForSeconds(PieceSleepCheckDelay);
+        float activeRigidbodies = Rigidbodies.Length;
+
+        while (activeRigidbodies > 0)
+        {
+            yield return Wait;
+
+            foreach (Rigidbody rigidbody in Rigidbodies)
+            {
+                if (rigidbody.IsSleeping())
+                {
+                    activeRigidbodies--;
+                }
+            }
         }
 
-        Destroy(parent);
+
+        yield return new WaitForSeconds(PieceDestroyDelay);
+
+        float time = 0;
+        Renderer[] renderers = Array.ConvertAll(Rigidbodies, GetRendererFromRigidbody);
+
+        foreach (Rigidbody body in Rigidbodies)
+        {
+            Destroy(body.GetComponent<Collider>());
+            Destroy(body);
+        }
+
+        while (time < 1)
+        {
+            float step = Time.deltaTime * PieceFadeSpeed;
+            foreach (Renderer renderer in renderers)
+            {
+                renderer.transform.Translate(Vector3.down * (step / renderer.bounds.size.y), Space.World);
+            }
+
+            time += step;
+            yield return null;
+        }
+
+        foreach (Renderer renderer in renderers)
+        {
+            Destroy(renderer.gameObject);
+        }
+        Destroy(gameObject);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private Renderer GetRendererFromRigidbody(Rigidbody Rigidbody)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return Rigidbody.GetComponent<Renderer>();
     }
 }
