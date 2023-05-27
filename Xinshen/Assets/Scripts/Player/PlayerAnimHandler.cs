e46b2e0ac2feb874d98bd9fdab4e375709c94792
Do3naut <51458829+Do3naut@users.noreply.github.com>
2023-05-27 16:25:49 -0700

    attacking physics lock

--- Xinshen/Assets/Scripts/Player/PlayerAnimHandler.cs
+++ Xinshen/Assets/Scripts/Player/PlayerAnimHandler.cs
@@ -12,6 +12,8 @@ public class PlayerAnimHandler : MonoBehaviour
     [SerializeField] ParticleSystem rightLegTrail;
     PlayerWeapon weapon;
     bool canCancel = false;
+    bool lockPhysics = false;
+    Vector3 commandVelocity;
     Transform cam;
     Vector3 targOrientation;
 
@@ -29,6 +31,7 @@ public class PlayerAnimHandler : MonoBehaviour
         cam = GetComponent<FlowMove>().cam;
         weapon = weaponCollider.gameObject.GetComponent<PlayerWeapon>();
         targOrientation = Vector3.zero;
+        commandVelocity = Vector3.zero;
         originalHbWidth = weaponCollider.size.x;
         originalHbHeight = weaponCollider.size.y;
         originalHbLength = weaponCollider.size.z;
@@ -65,6 +68,16 @@ public class PlayerAnimHandler : MonoBehaviour
         {
             targOrientation = Vector3.zero;
         }
+
+        if (lockPhysics)
+        {
+            rb.velocity = commandVelocity;
+        }
+    }
+    
+    public void LockPhysics(bool locked)
+    {
+        lockPhysics = locked;
     }
 
     // Adjusts the dimensions of the hitbox as a percent of the original. Hitbox will always be adjusted to be centered in front of player.
@@ -129,6 +142,7 @@ public class PlayerAnimHandler : MonoBehaviour
     public void SetFwdVelocity(float vel)
     {
         rb.velocity = transform.forward * vel;
+        commandVelocity = transform.forward * vel;
     }
 
     public void OrientTowardsInput()
