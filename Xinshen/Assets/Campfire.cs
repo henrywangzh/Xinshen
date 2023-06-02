9b2eaf338de114e67e4b4290653f80a4d27cc031
Kaiway Tang <86494513+kaiway-tang@users.noreply.github.com>
2023-06-02 02:11:12 -0700

    campfire, procedural room gen
    procedural room gen using "chunk" system; not finished

---
+++ Xinshen/Assets/Campfire.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campfire : Interactable
+{
+    [SerializeField] ParticleSystem sparks, flames;
+    int channelTimer;
+    bool isHealing;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isInteractable)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                if (channelTimer > 199)
+                {
+                    isHealing = true;
+                    flames.Play();
+                }
+                else if (channelTimer % 50 == 0)
+                {
+                    sparks.Play();
+                }
+
+                channelTimer++;
+            }
+            else
+            {
+                channelTimer = 0;
+            }
+
+            if (isHealing)
+            {
+                PlayerHP.Heal(1);
+            }
+        } else if (isHealing)
+        {
+            isHealing = false;
+            flames.Stop();
+        }
+    }
+
+protected override void Interact()
+    {
+
+    }
+}
